31ba989f9073154b68920deaf5cc0aed180886cf
Venkatesh <vinodjc007@gmail.com>
2024-08-14 10:44:15 +0530

    added google analytics

--- app.py
+++ app.py
@@ -115,6 +115,15 @@ def index():
             }
         </style>
     </head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FH4VDTEZ0W"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FH4VDTEZ0W');
+</script>
     <body>
         <div class="container">
             <div class="header">1080 Quality</div>
